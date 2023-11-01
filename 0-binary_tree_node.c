ea5b5a8905802ab89888ea51279c2eb48ce233ad
Vulcanric <jneric49@gmail.com>
2023-11-01 11:29:42 +0100

    Create function that creates a binary tree node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,7 +13,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new_node = malloc(sizeof(binary_tree_t));
 	if (!new_node)
 	{
-		return NULL;
+		return (NULL);
 	}
 
 	/* childless by default */
