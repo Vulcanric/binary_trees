eb0a21f8ee0c29cdeeb544260c7df0e5a1449b8e
Vulcanric <jneric49@gmail.com>
2023-11-09 11:17:04 +0100

    Adds function that finds the lowest common ancestor of two nodes

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -6,7 +6,7 @@
  * @second: second node
  * Return: on success the lowest common ancestor, otherwise null
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, \
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second)
 {
 	binary_tree_t *a1, *a2;
@@ -16,10 +16,7 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, \
 		for (a2 = (binary_tree_t *)second; a2 != NULL; a2 = a2->parent)
 		{
 			if (a1->n == a2->n)
-			{
-				/* Change const qualifier and return */
 				return (a1);
-			}
 		}
 	}
 
